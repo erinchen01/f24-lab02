ae5bd81219f7331faa52d78ce0c6e408b5c3db20
Xinyi Chen <xinyic2@andrew.cmu.edu>
2024-09-06 12:09:55 -0400

    enabled only shape interface in main

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -2,15 +2,17 @@ import { newRenderer } from "./renderer.js"
 import { Rectangle, newRectangle } from "./shapes/rectangle.js";
 import { circle } from "./shapes/circle.js";
 import { square } from "./shapes/square.js";
+import { Shape } from "./shapes/shape.js";
 
-const rectangle: Rectangle = newRectangle(2, 3)
+
+const rectangle: Shape = newRectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
 
-const circleShape = circle(5);
+const circleShape: Shape = circle(5);
 const circleRenderer = newRenderer(circleShape)
 circleRenderer.draw();
 
-const squareShape = square(5);
+const squareShape: Shape = square(5);
 const squareRenderer = newRenderer(squareShape)
 squareRenderer.draw();
