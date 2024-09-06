e8a584b3225115f40165fc3a3ef98eab43ca75d6
Xinyi Chen <xinyic2@andrew.cmu.edu>
2024-09-06 11:57:26 -0400

    enabled all uses of shape in ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,16 @@
 import { newRenderer } from "./renderer.js"
 import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { circle } from "./shapes/circle.js";
+import { square } from "./shapes/square.js";
 
 const rectangle: Rectangle = newRectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
+
+const circleShape = circle(5);
+const circleRenderer = newRenderer(circleShape)
+circleRenderer.draw();
+
+const squareShape = square(5);
+const squareRenderer = newRenderer(squareShape)
+squareRenderer.draw();
