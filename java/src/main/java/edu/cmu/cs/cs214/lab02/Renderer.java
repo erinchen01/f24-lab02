991ec2571ea6aa0a3db50ef4529863d490fa8941
Xinyi Chen <xinyic2@andrew.cmu.edu>
2024-09-06 11:22:47 -0400

    encapsulated variables and updated Shape interface

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Renderer.java
@@ -3,14 +3,15 @@ package edu.cmu.cs.cs214.lab02;
 import edu.cmu.cs.cs214.lab02.shapes.*;
 
 public class Renderer {
-    public Rectangle rectangle;
-    
-    Renderer(Rectangle rectangle) {
-        this.rectangle = rectangle;
+    // public Rectangle rectangle;
+    public Shape shape;
+
+    Renderer(Rectangle shape) {
+        this.shape = shape;
     }
 
     void draw() {
-        double area = rectangle.getArea();
+        double area = shape.getArea();
 
         // assume implementation
 
